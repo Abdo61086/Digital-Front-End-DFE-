fc48308ce76133965bc8977e9782ae07db98f470
Ahmed Ibrahim <157659381+ahmedg3x1@users.noreply.github.com>
2025-11-21 01:17:15 +0200

    edit clk divider design

--- RTL/TOP/clk_div.v
+++ RTL/TOP/clk_div.v
@@ -1,28 +1,25 @@
 module clk_div(
     input      clk_in,
     input      rst_n,
-    output reg clk_out
+    output     clk_out
 );
 
     reg [1:0] counter;
 
+    assign clk_out = (counter > 2'b01);
+
     always @(posedge clk_in, negedge rst_n) begin
         if (~rst_n) begin
-            counter <= 1;
-            clk_out <= 0;
+            counter <= 0;
         end 
         else begin
-            if(counter == 2'b0) begin
-                clk_out <= ~clk_out;
-                counter <= counter + 1;
-            end
-            else if (counter == 2'b10) begin
-                clk_out <= ~clk_out;
+            if (counter == 2'b10) begin
                 counter <= 0;
             end 
             else 
                 counter <= counter + 1;
         end
+        
     end
 
 endmodule
